94f7e41954fd4f7b20b89225df9613753d7c09bc
Vladimir Bugaev <boogvar@gmail.com>
2025-10-07 21:26:12 +0700

    update build

--- library/std/build.rs
+++ library/std/build.rs
@@ -49,7 +49,6 @@ fn main() {
         || target_os == "uefi"
         || target_os == "teeos"
         || target_os == "zkvm"
-        || target_os == "solana"
         || target_os == "rtems"
         || target_os == "nuttx"
         || target_os == "cygwin"
